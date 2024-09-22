e49469083cc6ab7c27995f72d13db6a844d40b98
nessamisiti <vanessapereiraalmeida29@gmail.com>
2024-09-22 14:21:40 -0300

    Função do cálculo e comentários pra descrever melhor as outras funções

--- calculadora.js
+++ calculadora.js
@@ -1,12 +1,14 @@
-// Funções da calculadora
+//Função para operação de soma
 function somar(a, b) {
     return a + b;
 }
 
+//Função para operação de multiplicação
 function multiplicar(a, b) {
     return a * b;
 }
 
+//Função para operação de divisão
 function dividir(a, b) {
     if (b === 0) {
         return "Erro: Divisão por zero!";
@@ -14,11 +16,28 @@ function dividir(a, b) {
     return a / b;
 }
 
+//Função para validar número inserido
 function validarNumero(numero) {
     const naoEhNumero = isNaN(numero);
     if(naoEhNumero === true) {
         return false;
     } else {
         return true;
+    }
+}
+
+// Função para calcular com base na operação
+function calcular(operacao, a, b) {
+    switch (operacao) {
+        case '+':
+            return somar(a, b);
+        case '-':
+            return subtrair(a, b);
+        case '*':
+            return multiplicar(a, b);
+        case '/':
+            return dividir(a, b);
+        default:
+            return "Operação inválida!";
     }
 }
